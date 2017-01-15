e04c75f4a0512bc8d58d02dc98401a3b4e90e065
gio_priv <giovanniprivit@gmail.com>
2017-01-15 20:39:27 +0100

    constants added

--- constants.py
+++ constants.py
@@ -1,4 +1,54 @@
 """
 Constants
 """
+ght speed
+cs=3.438*10**4                           #Sound speed in the air
+cs_T10_4=1.0*10**5                    #Sound speed in the interstellar medium at T=10**4K
+G=6.67428*10**(-8)                       #Gravitational constant
+h=6.62606896*10**(-27)
+k=1.3806504*10**(-16)                    #Boltzmann constant
+a=7.56576738*10**(-15)
+rgaz=8.314472*10**7                                #Gas constant
+sigma=5.67040*10**(-5)
+me=9.1093826*10**(-28)
+avo=6.02214179*10**23
+u=1.660538782*10**(-24)
+mh=1.67372346*10**(-24)
+thomson=6.652458558*10**25
+#! charge de l'electron en coulomb.
+e=1.602176487*10**(-19)
+ecgs=4.8032068*10**(-10)
+#! qapicg: 4*pi*c*G
+qapicg=2.514403597*10**4
+#! K1= (1/5)*(3/8pi)**(2/3)*h**2/(m_e*h**(-5/3))
+K1=9.844840461*10**12
+Msol=1.9884*10**33
+Rsol=6.9551*10**10
+gsol=2.7434*10**4
+Lsol=3.8427*10**33
+Mju=1.8986*10**30
+Mea=5.972*10**27
+xlsomo=1.932558841
+uastr=1.49597870660*10**13
+year=3.15569252*10**7
+lgMsol=33.29850375
+lgRsol=10.84229713
+lgLsol=33.58460257
+um=2.3025850929940457
+Q_H=26.229
+Q_He=7.274
+Q_C=4.617
+convMeVerg=1.602176487*10**(-6)
+uma=1.660538921*10**-24
+deltaang=0.980
 
+##########################################################################################
+v_wind=500000                                       # Wind velocity in the red giant[cm s-1]
+cs_T2500=3.2*10**5                                  # Sound speed with T=2500  [cm s-1]
+cs_T6500=7.0*10**5                                  # In AGB star 7kms-1
+C_d=0.9                                             # Coeff aerod di resistenza
+I=0.5                                               # Function of Mach number
+
+    
+    
+    
